c20a70b07d215a5a996aa4fb48856bed2b8b8271
Gabriel Ronei <gabriheron@hotmail.com>
2024-03-20 22:04:47 -0300

    refat(algorithms): melhorando solucao do between two sets \o/

--- src/main/java/br/com/gabriels/algorithms/implementation/betweenTwoSets/Solution.java
+++ src/main/java/br/com/gabriels/algorithms/implementation/betweenTwoSets/Solution.java
@@ -1,11 +1,11 @@
 package br.com.gabriels.algorithms.implementation.betweenTwoSets;
 
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
+//https://www.hackerrank.com/challenges/between-two-sets
 class Result {
 
     /*
@@ -17,41 +17,99 @@ class Result {
      *  2. INTEGER_ARRAY b
      */
 
+//    public static int getTotalX(List<Integer> a, List<Integer> b) {
+//    // Write your code here
+//        HashSet<Object> aList = new HashSet<>();
+//        HashSet<Object> bList = new HashSet<>();
+//
+//
+//        for (Integer integer: a) {
+//            HashSet<Integer> objects = new HashSet<>();
+//            for(int i=1; i<=100; i++) {
+//                int result = integer * i;
+//                objects.add(result);
+//            }
+//            if (aList.isEmpty()) aList.addAll(objects);
+//            else aList.retainAll(objects);
+//        }
+//
+//        System.out.println(aList);
+//
+//        for (Integer integer: b) {
+//            HashSet<Integer> objects = new HashSet<>();
+//            for(int i=1; i<=100; i++) {
+//                boolean result = integer % i == 0;
+//
+//                if (result) objects.add(i);
+//            }
+//            if (bList.isEmpty()) bList.addAll(objects);
+//            else bList.retainAll(objects);
+//        }
+//
+//        System.out.println(bList);
+//
+//        aList.retainAll(bList);
+//        return aList.size();
+//    }
+
     public static int getTotalX(List<Integer> a, List<Integer> b) {
-    // Write your code here
-        HashSet<Object> aList = new HashSet<>();
-        HashSet<Object> bList = new HashSet<>();
+        // Write your code here
+        Set<Integer> multiplos = encontrarMultiplos(a);
+        Set<Integer> divisores = encontraDivisores(b);
 
+        multiplos.retainAll(divisores);
 
-        for (Integer integer: a) {
-            HashSet<Integer> objects = new HashSet<>();
-            for(int i=1; i<=100; i++) {
-                int result = integer * i;
-                objects.add(result);
-            }
-            if (aList.isEmpty()) aList.addAll(objects);
-            else aList.retainAll(objects);
-        }
-
-        System.out.println(aList);
-
-        for (Integer integer: b) {
-            HashSet<Integer> objects = new HashSet<>();
-            for(int i=1; i<=100; i++) {
-                boolean result = integer % i == 0;
-
-                if (result) objects.add(i);
-            }
-            if (bList.isEmpty()) bList.addAll(objects);
-            else bList.retainAll(objects);
-        }
-
-        System.out.println(bList);
-
-        aList.retainAll(bList);
-        return aList.size();
+        return multiplos.size();
     }
 
+    private static Set<Integer> encontraDivisores(List<Integer> numeros) {
+        int mdc = numeros.get(0);
+
+        for (int i = 1; i < numeros.size(); i++) {
+            mdc = calculaMDC(mdc, numeros.get(i));
+        }
+
+        Set<Integer> divisores = new HashSet<>();
+        for (int i = 1; i <= mdc; i++) {
+            if (mdc % i == 0) {
+                divisores.add(i);
+            }
+        }
+
+        System.out.println(divisores);
+        return divisores;
+    }
+
+    private static Set<Integer> encontrarMultiplos(List<Integer> numeros) {
+
+        int mmc = numeros.get(0);
+
+        for (int i = 1; i < numeros.size(); i++) {
+            mmc = calculaMMC(mmc, numeros.get(i));
+        }
+
+        Set<Integer> multiplos = new HashSet<>();
+        for (int i = 1; i <= 100; i++) {
+            if (i % mmc == 0) {
+                multiplos.add(i);
+            }
+        }
+
+        System.out.println(multiplos);
+        return multiplos;
+    }
+
+    private static Integer calculaMMC(int a, int b) {
+        return a * (b / calculaMDC(a, b));
+    }
+
+    private static Integer calculaMDC(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        return calculaMDC(b, a % b);
+    }
 }
 
 public class Solution {
@@ -63,15 +121,14 @@ public class Solution {
         int m = Integer.parseInt(firstMultipleInput[1]);
 
         List<Integer> arr = Stream.of("2 4".split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+                .map(Integer::parseInt)
+                .collect(toList());
 
         List<Integer> brr = Stream.of("16 32 96".split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+                .map(Integer::parseInt)
+                .collect(toList());
 
         int total = Result.getTotalX(arr, brr);
         System.out.println(total);
-//        System.out.println(24 % 2 == 0);
     }
 }
